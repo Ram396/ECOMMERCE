87b31d43499d22b5f4e5e476b2321ff15d140196
ram <rakhip396@gmail.com>
2023-04-17 00:04:27 +0530

    first commit

---
+++ e-commerce-backend/controllers/product.controller.js
@@ -0,0 +1,155 @@
+const Product = require('../models/product.model');
+
+exports.getAllProducts = async (req, res) => {
+    try {
+        const size = await Product.countDocuments();
+        if (size === 0) {
+            res.json({ message: 'No products found' });
+        }
+        if (req.query.category) {
+            const products = await Product.find({ category: req.query.category.charAt(0).toUpperCase() + req.query.category.slice(1) });
+            res.json(products);
+        } else {
+            const products = await Product.find();
+            res.json(products);
+        }
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+exports.getProductById = async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+        if (product == null) {
+            return res.status(404).json({ message: 'Cannot find product' });
+        }
+        res.json(product);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+exports.createProduct = async (req, res) => {
+    const product = new Product({
+        name: req.body.name,
+        brand: req.body.brand,
+        category: req.body.category,
+        stock: req.body.stock,
+        discount: req.body.discount,
+        price: req.body.price,
+        description: req.body.description,
+        image: req.body.image,
+        category: req.body.category,
+        tags: req.body.tags,
+        rating: req.body.rating,
+        totalSales: req.body.totalSales,
+    });
+    try {
+        await product.save();
+        res.status(201).json({ message: 'Product Saved' });
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+};
+
+exports.createManyProducts = async (req, res) => {
+    try {
+        await Product.insertMany(req.body);
+        res.status(201).json({ message: 'Products Saved' });
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+};
+
+exports.deleteProduct = async (req, res) => {
+    try {
+        const id = req.params.id;
+        await Product.findByIdAndDelete(id);
+        res.json({ message: 'Product deleted' });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+exports.updateProduct = async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    if (product == null) {
+        return res.status(404).json({ message: 'Cannot find product' });
+    }
+    if (req.body.name != null) {
+        product.name = req.body.name;
+    }
+    if (req.body.brand != null) {
+        product.brand = req.body.brand;
+    }
+    if (req.body.category != null) {
+        product.category = req.body.category;
+    }
+    if (req.body.stock != null) {
+        product.stock = req.body.stock;
+    }
+    if (req.body.discount != null) {
+        product.discount = req.body.discount;
+    }
+    if (req.body.price != null) {
+        product.price = req.body.price;
+    }
+    if (req.body.description != null) {
+        product.description = req.body.description;
+    }
+    if (req.body.image != null) {
+        product.image = req.body.image;
+    }
+    if (req.body.category != null) {
+        product.category = req.body.category;
+    }
+    if (req.body.tags != null) {
+        product.tags = req.body.tags;
+    }
+    if (req.body.rating != null) {
+        product.rating = req.body.rating;
+    }
+    if (req.body.totalSales != null) {
+        product.totalSales = req.body.totalSales;
+    }
+    try {
+        await product.save();
+        res.json({ message: 'Product updated' });
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+};
+
+exports.deleteAllProducts = async (req, res) => {
+    try {
+        const size = await Product.countDocuments();
+        if (size === 0) {
+            res.json({ message: 'No products found' });
+        } else {
+            await Product.deleteMany();
+            res.json({
+                message: 'Deleted all products',
+                status: 'success',
+                size,
+            });
+        }
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+exports.searchProducts = async (req, res) => {
+    try {
+        const size = await Product.countDocuments();
+        if (size === 0) {
+            res.json({ message: 'No products found' });
+        }
+        const products = await Product.find({
+            name: { $regex: req.params.query, $options: 'i' },
+        });
+        res.json(products);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
